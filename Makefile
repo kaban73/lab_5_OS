55b0ecfc898a4881fd74a80d53fef842c7d566af
ivankabanov <i.borkoff@yandex.ru>
2024-10-31 18:50:06 +0400

    init

---
+++ Makefile
@@ -0,0 +1,12 @@
+all: libsimplefs.a app
+
+libsimplefs.a: simplefs.c
+	gcc -Wall -c simplefs.c
+	ar -cvq libsimplefs.a simplefs.o
+	ranlib libsimplefs.a
+
+app: main.c
+	gcc -Wall -o app app.c  -L. -lsimplefs
+
+clean:
+	rm -fr *.o *.a *~ a.out app  vdisk1.bin
---
+++ simplefs.c
@@ -0,0 +1,134 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include "simplefs.h"
+
+int vdisk_fd; // global virtual disk file descriptor
+              // will be assigned with the sfs_mount call
+              // any function in this file can use this.
+
+
+// This function is simply used to a create a virtual disk
+// (a simple Linux file including all zeros) of the specified size.
+// You can call this function from an app to create a virtual disk.
+// There are other ways of creating a virtual disk (a Linux file)
+// of certain size.
+// size = 2^m Bytes
+int create_vdisk (char *vdiskname, int m)
+{
+    char command[BLOCKSIZE];
+    int size;
+    int num = 1;
+    int count;
+    size  = num << m;
+    count = size / BLOCKSIZE;
+    printf ("%d %d", m, size);
+    sprintf (command, "dd if=/dev/zero of=%s bs=%d count=%d", vdiskname, BLOCKSIZE, count);
+    printf ("executing command = %s\n", command);
+    system (command);
+    return (0);
+}
+
+
+
+// read block k from disk (virtual disk) into buffer block.
+// size of the block is BLOCKSIZE.
+// space for block must be allocated outside of this function.
+// block numbers start from 0 in the virtual disk.
+int read_block (void *block, int k)
+{
+    int n;
+    int offset;
+
+    offset = k * BLOCKSIZE;
+    lseek(vdisk_fd, (off_t) offset, SEEK_SET);
+    n = read (vdisk_fd, block, BLOCKSIZE);
+    if (n != BLOCKSIZE) {
+        printf ("read error\n");
+        return -1;
+    }
+    return (0);
+}
+
+// write block k into the virtual disk.
+int write_block (void *block, int k)
+{
+    int n;
+    int offset;
+
+    offset = k * BLOCKSIZE;
+    lseek(vdisk_fd, (off_t) offset, SEEK_SET);
+    n = write (vdisk_fd, block, BLOCKSIZE);
+    if (n != BLOCKSIZE) {
+        printf ("write error\n");
+        return (-1);
+    }
+    return 0;
+}
+
+
+/**********************************************************************
+   The following functions are to be called by applications directly.
+***********************************************************************/
+
+int sfs_format (char *vdiskname)
+{
+    return (0);
+}
+
+int sfs_mount (char *vdiskname)
+{
+    // simply open the Linux file vdiskname and in this
+    // way make it ready to be used for other operations.
+    // vdisk_fd is global; hence other function can use it.
+    vdisk_fd = open(vdiskname, O_RDWR);
+    return(0);
+}
+
+int sfs_umount ()
+{
+    fsync (vdisk_fd);
+    close (vdisk_fd);
+    return (0);
+}
+
+
+int sfs_create(char *filename)
+{
+    return (0);
+}
+
+
+int sfs_open(char *file, int mode)
+{
+    return (0);
+}
+
+int sfs_close(int fd){
+    return (0);
+}
+
+int sfs_getsize (int  fd)
+{
+    return (0);
+}
+
+int sfs_read(int fd, void *buf, int n){
+    return (0);
+}
+
+
+int sfs_append(int fd, void *buf, int n)
+{
+    return (0);
+}
+
+int sfs_delete(char *filename)
+{
+    return (0);
+}
+
